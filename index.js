d921179542edb81c1b70332f3973eb809e215c34
Baker <baker@shamlan.sa>
2023-05-13 12:25:12 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-const { Client, Intents } = require('discord.js');
-const DiscordOccupancySensor = require('./lib/discord.js');
-const PLUGIN_NAME = 'homebridge-discord-presence';
-const PLATFORM_NAME = 'DiscordPresence';
+const PLUGIN_NAME = 'homebridge-discord-occupancy-sensor';
+const PLATFORM_NAME = 'DiscordOccupancySensor';
+const DiscordObserver = require('./lib/discord');
 
-module.exports = (api) => {
-  api.registerPlatform(PLUGIN_NAME, PLATFORM_NAME, DiscordPresence);
+module.exports = api => {
+  api.registerPlatform(PLUGIN_NAME, PLATFORM_NAME, DiscordOccupancySensor);
 };
 
-class DiscordPresence {
+class DiscordOccupancySensor {
   constructor(log, config, api) {
-    this.api = api;
     this.log = log;
-
-    this.accessories = [];
+    this.devicesConfig = config.devices || [];
+    this.name = config.name || PLATFORM_NAME;
+    this.interval = config.interval ? config.interval * 1000 : 10000;
+    this.threshold = !config.threshold && config.threshold !== 0 ? 15 : config.threshold;
     this.PLUGIN_NAME = PLUGIN_NAME;
     this.PLATFORM_NAME = PLATFORM_NAME;
-    this.name = config.name || PLATFORM_NAME;
-    this.token = config.token;
-    this.guildId = config.guildId;
+    this.accessories = [];
     this.debug = config.debug || false;
+    this.discordObserver = new DiscordObserver(config, this);
+    this.api = api;
 
     // define debug method to output debug logs when enabled in the config
     this.log.easyDebug = (...content) => {
@@ -33,67 +33,13 @@ class DiscordPresence {
       }
     };
 
-    // Create a new Discord client with the proper intents
-    this.client = new Client({
-      intents: [
-        Intents.FLAGS.GUILDS,
-        Intents.FLAGS.GUILD_MEMBERS,
-        Intents.FLAGS.PRESENCE_UPDATE
-      ]
-    });
-    // this.client = new Discord.Client();
-    this.client.on('ready', this.onReady.bind(this));
-    
-    this.client.login('your-bot-token').then(() => {
-      this.log('Discord bot is ready');
-    }).catch(error => {
-      this.log.error(`Failed to log in to Discord: ${error}`);
-    });
-
-    this.api.on('didFinishLaunching', () => {
-      this.client.login(this.token);
-    });
-  }
-
-  onReady() {
-    this.log(`Logged in as ${this.client.user.tag}`);
-    const guild = this.client.guilds.cache.get(this.guildId);
-    if (!guild) {
-      this.log.error(`Failed to find guild with ID ${this.guildId}`);
-      return;
-    }
-
-    // Remove any cached accessories that are no longer in the guild
-    this.accessories.forEach((accessory) => {
-      const serial = accessory.context.serial;
-      if (!guild.members.cache.has(serial)) {
-        this.log(`Removing cached accessory ${accessory.displayName} (${serial})`);
-        this.api.unregisterPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
-      }
-    });
-
-    guild.members.fetch()
-      .then((members) => {
-        members.each((member) => {
-          if (this.accessories.some((accessory) => accessory.context.serial === member.id)) {
-            // Accessory already exists for this member, skip
-            return;
-          }
-
-          this.log(`Adding accessory for ${member.displayName} (${member.id})`);
-          const accessory = new DiscordOccupancySensor(guild, {
-            name: member.displayName,
-          }, this);
-          this.accessories.push(accessory.accessory);
-        });
-      })
-      .catch((error) => {
-        this.log.error(`Failed to fetch guild members: ${error}`);
-      });
+    this.api.on('didFinishLaunching', this.discordObserver.init.bind(this.discordObserver));
   }
 
   configureAccessory(accessory) {
-    this.log.easyDebug(`Found Cached Accessory: ${accessory.displayName} (${accessory.context.serial}) `);
+    this.log.easyDebug(`Found cached accessory: ${accessory.displayName} (${accessory.context.serial})`);
     this.accessories.push(accessory);
   }
 }
+
+module.exports = DiscordOccupancySensor;
